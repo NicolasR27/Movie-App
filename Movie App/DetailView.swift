c94addd7d877e337a5ae575c5f96a119588f001a
Nicolas Rios <nickrios33@yahoo.com>
2020-02-12 22:30:39 -0800

    added H stack and scroll View

--- Movie App/DetailView.swift
+++ Movie App/DetailView.swift
@@ -1,39 +1,33 @@
-//
-//  DetailView.swift
-//  Movie App
-//
-//  Created by Nicolas Rios on 2/12/20.
-//  Copyright © 2020 Nicolas Rios. All rights reserved.
-//
+
 
 import SwiftUI
 
 struct DetailView: View {
     var body: some View {
-        VStack {
+        ScrollView(.vertical) {
             PosterView()
             TitleView()
             FilmInfo()
+            RatingView()
+            PlotView()
             
             HStack {
-                ForEach(0 ..< 3) { item in
-                    Image(systemName: "star.fill")
+                Text("Cast")
+                Spacer()
+                Button(action:{
+                  //
+                }) {
+                    Text("See all")
+                    
                 }
-                
-                Image(systemName: "star.lefthalf.fill")
-                Image(systemName: "star")
-                
-                Text("(3.5)")
-                .bold()
-                    .padding(.leading)
-                    Spacer()
-            }
-             .foregroundColor(.yellow)
-            
-        }
+                .padding()
+                .foregroundColor(.secondary)
+                .clipShape(Capsule())
     }
-}
 
+    }
+  }
+}
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
@@ -68,5 +62,42 @@ struct FilmInfo: View {
                 .foregroundColor(.secondary)
                 .padding(.leading)
         }
+    }
+}
+
+struct RatingView: View {
+    var body: some View {
+        HStack {
+            ForEach(0 ..< 3) { item in
+                Image(systemName: "star.fill")
+            }
+            
+            Image(systemName: "star.lefthalf.fill")
+            Image(systemName: "star")
+            
+            Text("(3.5)")
+                .bold()
+                .padding(.leading)
+            Spacer()
+        }
+        .padding(.leading)
+        .foregroundColor(.yellow)
+    }
+}
+
+struct PlotView: View {
+    var body: some View {
+        VStack{
+            HStack {
+                Text("Storyline")
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding(.bottom)
+            
+            Text(" All seems well on the island of Berk as Vikings and dragons live together in peace and harmony. Now a Viking leader, Hiccup finds himself increasingly attracted to Astrid, while his beloved dragon Toothless meets an enchanting creature who captures his eye. When the evil Grimmel launches a devious plan to wipe out all the dragons, Hiccup must unite both clans to find Caldera, a hidden land that holds the key to saving Toothless and his flying friends.")
+            
+        }
+        .padding()
     }
 }
